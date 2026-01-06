8c98b5a0b3168ecb8786c323228d22c4f89ea271
Hernan <156428836+HernanMA@users.noreply.github.com>
2026-01-05 21:14:05 -0500

    🚀 Kotlin text game

---
+++ KoltinForJavaDevelopers/kotlinTextAdventure/src/kotlinText/Main.kt
@@ -0,0 +1,33 @@
+package com.kotlinText
+
+fun main(args: Array<String>) {
+
+    val locations = readLocationInfo()
+    var loc = 64
+
+    while (true) {
+
+        val location = locations[loc] ?: Location(0,
+                                        "Something went wrong, The game will terminate")
+        println(location.description)
+
+        if (location.locationID == 0) {
+            break
+        }
+
+        println("Available exits are: ")
+        location.exits.keys.forEach {
+            println("$it, ")
+        }
+
+        val direction = readlnOrNull()?.toUpperCase() ?: "Z"
+        if (location.exits.containsKey(direction)) {
+            loc = location.exits[direction]!!
+        }
+        else {
+            println("Unknown direction")
+        }
+    }
+
+}
+
